538f058ae45965127f8f0bee880b81cbd6f5960e
Roman Sotnikov <axeman.ofic@gmail.com>
2023-10-20 19:11:13 +0300

    feat: add toggling terminal

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -2,8 +2,20 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<C-\\>"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+		},
+	},
+  t = {
+    ["<C-\\>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+    },
   },
 }
 
